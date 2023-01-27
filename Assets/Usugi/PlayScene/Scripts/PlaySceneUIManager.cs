2bf486992e55714fd76cd997a2f0f227e725b956
TsuyosiUsugi <qesulive@gmail.com>
2023-01-27 11:35:37 +0900

    Revert "[fix]コンフリクト解消中"
    This reverts commit e6465b3b3bc56d186707cf1c334075a3f66054d7.

--- Assets/Usugi/PlayScene/Scripts/PlaySceneUIManager.cs
+++ Assets/Usugi/PlayScene/Scripts/PlaySceneUIManager.cs
@@ -41,7 +41,7 @@ public class PlaySceneUIManager : MonoBehaviour
     void ReadyUIAnim()
     {
         _howToStartButton.gameObject.SetActive(true);
-        _howToStartButton.DOFade(0, 1f).SetLoops(-1, LoopType.Yoyo);
+        _howToStartButton.DOFade(0, 1f).SetLoops(-1, LoopType.Yoyo).SetLink(_howToStartButton.gameObject);
     }
 
     void CombUICount(int comb)
@@ -66,7 +66,7 @@ public class PlaySceneUIManager : MonoBehaviour
                 score,
                 0.5f
             )
-            .OnUpdate(() => _scoreCount.text = $"SCORE:{_score.ToString("00000")}");
+            .OnUpdate(() => _scoreCount.text = $"SCORE:{_score.ToString("00000")}").SetLink(_scoreCount.gameObject);
     }
 
 }
